22e25057818337b1cee010b74cf37fec17c4ed2d
Mohit <mohitd2288@gmail.com>
2024-10-17 01:19:37 +0530

    feat: add index.js

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectToDB = require("./utils/connectToDB");
+
+// routes
+const v1Router = require("./routes");
+
+dotenv.config();
+
+const CLIENT_URL = process.env.CLIENT_URL;
+const SERVER_PORT = process.env.SERVER_PORT;
+const DB_URL = process.env.DB_URL;
+const app = express();
+
+connectToDB(DB_URL);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: CLIENT_URL
+}))
+
+app.use("/api/v1", v1Router);
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+});
+
+const port = process.env.SERVER_PORT
+
