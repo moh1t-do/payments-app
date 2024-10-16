5fbe937812c30924a8fa713383864fb441ba38f0
Mohit <mohitd2288@gmail.com>
2024-10-17 01:23:09 +0530

    feat: add auth middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const authMiddleware = async (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if (!header || !header.startsWith('Bearer ')) {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = header.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+        if (error) {
+            res.status(401).json({ error: 'Unauthorized' });
+        }
+        req.userid = decoded.userid;
+        next();
+    });
+}
+
+module.exports = authMiddleware;
