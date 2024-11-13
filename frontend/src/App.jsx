62a8588d7dc4bb6216896e357cdbbcc835c830ff
Mohit <mohitd2288@gmail.com>
2024-11-13 17:59:19 +0530

    feat: add new components for UI structure and user interaction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import { Route, Routes } from "react-router-dom"
-import Signup from "./pages/Signup"
-import Signin from "./pages/Signin"
-import Dashboard from "./pages/Dashboard"
-import SendMoney from "./pages/SendMoney"
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup.jsx";
+import { Signin } from './pages/Signin.jsx';
+import { Dashboard } from './pages/Dashboard.jsx';
+import { SendMoney } from './pages/SendMoney.jsx';
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/send" element={<SendMoney/>} />
-    </Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/send' element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
-function Dashboard() {
-    return ( <div>
-        Dashboard
-    </div> );
-}
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
 
-export default Dashboard;
+export const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
+}
